2f751dc4294d452e8dac968d119df6313c6a9696
vansh <pawarsmita12345678@gmail.com>
2022-06-15 16:16:13 +0530

    smita_self

---
+++ Git_self/src/com/cjc/smita.java
@@ -0,0 +1,21 @@
+package com.cjc;
+
+public class smita
+{
+	public void m1()
+	{
+		System.out.println("-----GIT_Proccess--");
+	}
+	
+		public static void main(String[] args)
+		{
+			System.out.println("smita----------Hello");
+			System.out.println("smita----------cjc");
+			System.out.println("smita----------pune");
+			
+			System.out.println("smita-------cjc------Hello");
+		}
+
+	
+
+}
